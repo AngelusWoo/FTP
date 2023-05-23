36356d4aaaf8fa834e6966defa0f8af8dde2cccf
AngelusWoo <angelwoocastillo@gmail.com>
2023-05-23 14:59:41 +0200

    Initial Commit

---
+++ version18_Ref/src/constantes.ts
@@ -0,0 +1,33 @@
+const Constantes = {
+    EVENTOS:{
+        VIDAS: 'cambiaVidas',
+        PUNTUACION: 'cambiaPuntuacion'
+    },
+    MENU:{
+        JUGAR: 'Jugar'
+    },
+    HUD:{
+        VIDAS: 'Vidas:'
+    },    
+    ESCENAS:{
+        CARGA: 'Carga',
+        MENU: 'Menu',
+        NIVEL1: 'Nivel1',
+        HUD: 'HUD'
+    },
+    REGISTRO:{
+        VIDAS: 'vidas',
+        PUNTUACION: 'puntuacion'
+    }, 
+    MAPAS: {
+        NIVEL1:{
+            TILEMAPJSON: 'mapaNivel1',
+            CAPAPLATAFORMAS: 'Plataformas'
+        },
+        TILESET:'nivelestileset'
+    },
+    FONDOS:{
+        NIVEL1: 'Brown'
+    }
+};
+export default Constantes;
